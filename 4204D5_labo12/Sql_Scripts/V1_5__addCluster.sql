512886b00a02ef95a129ecfc17fc61852e268b6b
Bruno Pouliot <2168516@cegepmontpetit.ca>
2023-04-26 10:15:34 -0400

    Étape 4

---
+++ 4204D5_labo12/Sql_Scripts/V1_5__addCluster.sql
@@ -0,0 +1,12 @@
+USE Labo12
+GO
+
+DROP INDEX IF EXISTS Etudiants.Etudiant.IX_Etudiant_Nom_Prenom
+GO
+CREATE NONCLUSTERED INDEX IX_Etudiant_Nom_Prenom ON Etudiants.Etudiant(Prenom, Nom);
+GO
+
+DROP INDEX IF EXISTS Fruits.EtudiantFruit.IX_EtudiantFruit_FruitID
+GO
+CREATE NONCLUSTERED INDEX IX_EtudiantFruit_FruitID ON Fruits.EtudiantFruit(FruitID);
+GO
